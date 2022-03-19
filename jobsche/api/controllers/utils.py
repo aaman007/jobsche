9c58f15160c8b4dbd4a8562676e88a073ace8fa3
Amanur Rahman <aaman.sunny007@gmail.com>
2022-03-19 22:31:21 +0600

    Fix inconsistent-return-statements

--- jobsche/api/controllers/utils.py
+++ jobsche/api/controllers/utils.py
@@ -12,12 +12,12 @@ def safe_service_call(service_method, *args, **kwargs):
     try:
         return service_method(*args, **kwargs)
     except BadRequest as e:
-        abort(400, e.message)
+        return abort(400, e.message)
     except Forbidden as e:
-        abort(403, e.message)
+        return abort(403, e.message)
     except ObjectNotFound as e:
-        abort(404, message=e.message)
+        return abort(404, message=e.message)
     except PublicException as e:
-        abort(500, message=e.message)
+        return abort(500, message=e.message)
     except Exception as _:
-        abort(500, 'Something went wrong!')
+        return abort(500, 'Something went wrong!')
